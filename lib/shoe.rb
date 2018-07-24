2ed28239c190c54a0018181576ab4766b1130e2f
Roberto Garza <robertg4567@hotmail.com>
2018-07-24 17:59:28 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -3,7 +3,7 @@ class Shoe
   attr_reader :brand
 
   BRANDS = []
-  
+
   def initialize(brand)
     @brand = brand
   end
@@ -16,4 +16,5 @@ class Shoe
   def brand=(brand)
     @brand = brand
     BRANDS << brand
+  end 
 end
